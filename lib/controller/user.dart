609ae4dbe83757f5b35a194e82fb3801d9f88490
Benoit <fardouxbenoit@gmail.com>
2024-04-26 13:46:17 +0200

    int

---
+++ lib/controller/user.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_application_5/model/user.dart';
+
+class UserController {
+  final List<User> _users = [];
+  List<User> get users => _users;
+  Future<void> loadUsers() async {
+    users.add(User('Dupont', 'Jean'));
+    users.add(User('Durand', 'Pierre'));
+    users.add(User('Martin', 'Paul'));
+    users.add(User('Leclerc', 'Marie'));
+    users.add(User('Bertrand', 'Luc'));
+    users.add(User('Dupont', 'Jean'));
+    users.add(User('Durand', 'Pierre'));
+    users.add(User('Martin', 'Paul'));
+    users.add(User('Leclerc', 'Marie'));
+    users.add(User('Bertrand', 'Luc'));
+    users.add(User('Dupont', 'Jean'));
+    users.add(User('Durand', 'Pierre'));
+    users.add(User('Martin', 'Paul'));
+    users.add(User('Leclerc', 'Marie'));
+    users.add(User('Bertrand', 'Luc'));
+  }
+
+
+  void addUser(lastName, firstName) async{
+    User user = User(lastName, firstName);
+    users.add(user);
+  }
+}
